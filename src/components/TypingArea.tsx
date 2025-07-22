52285ee6a1c34da5677bb4e5c21a68818cf5cb83
Parth-2112 <raghuwanshiparth@gmail.com>
2025-07-22 21:50:58 +0530

    Made changes to header footer and added typing area

--- src/components/TypingArea.tsx
+++ src/components/TypingArea.tsx
@@ -1,10 +1,32 @@
+// import { useGameMode } from "../context/GameModeContext";
 
 const TypingArea = () => {
+    
+    // const {gameMode} = useGameMode();
+    const words = "This is a sample text"; // This should be dynamic based on game mode or other logic
+
     return(
-        <div className="border-2 border-amber-300 h-[50%] w-full mt-20 text-(--iconstext-color)">
-            TypingArea
+
+        <div className="flex-1 border-2 flex flex-col justify-center">
+            
+            <div className="timer text-(--secondary-color)">
+                timer
+            </div>
+            <div 
+                className="game border-2 border-amber-300 h-[105px] leading-[35px] w-full relative overflow-hidden " 
+                tabIndex={0}
+                >
+                <div className="words text-(--iconstext-color)">
+                    {words}     
+                </div>
+
+                <div className="focus-error absolute inset-0 flex items-center justify-center text-2xl text-(--iconstext-color-hover) border-2">
+                    click here to start typing
+                </div>
+            
+            </div>
         </div>
     );
-}
+} 
 
 export default TypingArea
